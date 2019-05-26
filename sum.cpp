22a60fb749cee81ee94dc26a5ea04845bc88eaff
ericwang12321 <ericwang12321@gmail.com>
2019-05-26 22:42:29 +0800

    first version of sum

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int sum = 0;
+	cout<<"Please enter 2 numbers:"<<endl;
+	int number1, number2;
+	cin>>number1>>number2;
+	sum = number1 + number2;
+	cout<<"The sum of these 2 numbers is "<<sum<<endl;
+} 
